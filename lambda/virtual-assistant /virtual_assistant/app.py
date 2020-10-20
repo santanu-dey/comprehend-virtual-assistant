1a96ce5ab10aeafaa58ff48878fd1cc6e02c244d
Santanu Dey <dey.santanu@gmail.com>
2020-10-20 18:15:08 +0800

    create lambda structure for deployment

--- lambda/virtual-assistant /virtual_assistant/app.py
+++ lambda/virtual-assistant /virtual_assistant/app.py
@@ -1,37 +1,16 @@
 import json
-
+import boto3
+import os
 # import requests
 
 
 def lambda_handler(event, context):
-    """Sample pure Lambda function
+    # Load environmental variables
+    env_region = os.environ['REGION']
+    email_text = os.environ['VAR1']
 
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
+    comprehend_results = invoke_comprehend(email_text, env_region)
 
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
 
     return {
         "statusCode": 200,
@@ -40,3 +19,10 @@ def lambda_handler(event, context):
             # "location": ip.text.replace("\n", "")
         }),
     }
+
+
+def invoke_comprehend(input_text, env_region):
+    # get the region of comprehend service   
+    comprehend_client = boto3.client(service_name='comprehend', region_name=env_region)
+
+    return "success"
